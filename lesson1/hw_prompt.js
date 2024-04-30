0a0f319b2c2c027b905ea7bc8bf0425642114c31
NadiyPro <siroviyn13@gmail.com>
2024-04-30 16:25:02 +0300

    create prompt

---
+++ lesson1/hw_prompt.js
@@ -0,0 +1,7 @@
+// // Додаткове для тих хто цікавився prompt`oм
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let firstName = prompt('what is your name?');
+let middleName = prompt('what is your patronymic?');
+let age = prompt('how old are you?');
+console.log(firstName,middleName,age);
