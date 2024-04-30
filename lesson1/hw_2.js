66a2d00a347d246299ed55f1eb8869e9c8e7abc8
NadiyPro <siroviyn13@gmail.com>
2024-04-30 16:06:35 +0300

    create hw2

---
+++ lesson1/hw_2.js
@@ -0,0 +1,8 @@
+ //    // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//     // З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+ let firstName = 'Nadia';
+ let middleName = 'Antonivna';
+ let lastName = 'Prosyanyk';
+ let person = `${lastName} ${firstName} ${middleName}`;
+ console.log(person);
