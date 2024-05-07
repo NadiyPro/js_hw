61e31feaf504ea0bddb3e0bc5385e4a64a5ab821
NadiyPro <siroviyn13@gmail.com>
2024-05-07 17:34:04 +0300

    create hw1

--- lesson3/hw1.js
+++ lesson3/hw1.js
@@ -1,24 +1,3 @@
-
-// // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-// // - Використовуючи данні з масиву, за допомоги document.write та циклу
-// // побудувати структуру по шаблону
-// // Масив:
-// //
-// //     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-// //
-// // ШАБЛОН:
-// //     <ul>
-// //         <li>ITEM OF ARRAY</li>
-// //         <!--
-// //             і тд інші об'єкти масиву
-// //              ...
-// //              ...
-// //              ...
-// //         -->
-// //     </ul>
-// // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
-
-
 // // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 let text = [
     'Lorem ipsum dolor sit amet.',
@@ -31,7 +10,7 @@ let text = [
     'Lorem ipsum dolor sit amet.',
     'Lorem ipsum dolor sit amet.',
     'Lorem ipsum dolor sit amet.'
-    ]
+    ];
 for (let i=0;i<10;i++){
     let blocks = text[i];
     document.write(
@@ -54,12 +33,12 @@ let textIndex = [
     'Lorem ipsum dolor sit amet.',
     'Lorem ipsum dolor sit amet.',
     'Lorem ipsum dolor sit amet.'
-]
+];
 for (let i=0;i<10;i++){
     document.write(
         `
         <div>
-        <p>${i} ${textIndex[i]}</p>
+        <p>${i+1} ${textIndex[i]}</p>
         </div>
         `
     );
@@ -87,16 +66,16 @@ let h = [
     'Lorem ipsum dolor sit amet, consectetur.',
     'Lorem ipsum dolor sit amet, consectetur.',
     'Lorem ipsum dolor sit amet, consectetur.'
-]
-let i=0;
-while (i<20){
-    let hText = h[i];
+];
+let j=0;
+while (j<20){
+    let hText = h[j];
     document.write(
         `
        <div><h1>${hText}</h1></div>
         `
     );
-    i++;
+    j++;
 }
 // // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 let hIndex = [
@@ -121,14 +100,42 @@ let hIndex = [
     'Lorem ipsum dolor sit amet, consectetur.',
     'Lorem ipsum dolor sit amet, consectetur.',
     'Lorem ipsum dolor sit amet, consectetur.'
-]
-let i=0;
-while (i<20){
-    let hTextIndex = hIndex[i];
+];
+let k=0;
+while (k<20){
     document.write(
         `
-       <div><h1>${i} ${hTextIndex}</h1></div>
+       <div><h1>${k+1} ${hIndex[k]}</h1></div>
         `
     );
-    i++;
+    k++;
+}
+// // - Використовуючи данні з масиву, за допомоги document.write та циклу
+// // побудувати структуру по шаблону
+// // Масив:
+// //
+// //     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// //
+// // ШАБЛОН:
+// //     <ul>
+// //         <li>ITEM OF ARRAY</li>
+// //         <!--
+// //             і тд інші об'єкти масиву
+// //              ...
+// //              ...
+// //              ...
+// //         -->
+// //     </ul>
+// // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+for(let l=0; l<listOfItems.length; l++){
+    document.write(
+        `
+    <ul>
+        <li>${listOfItems[l]}</li>
+    </ul>
+        `
+    )
 }
