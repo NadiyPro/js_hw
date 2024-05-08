c2dae46176f474759940ca04f96dc08e1ff3043e
NadiyPro <siroviyn13@gmail.com>
2024-05-08 15:06:05 +0300

    create additional3

--- lesson3/additional3.js
+++ lesson3/additional3.js
@@ -114,22 +114,22 @@ console.log(numberArray);
 // //         // TO BE CONTINUED .....
 // //     ]
 
-// let usersWithId = [
-//     {id: 1, name: 'vasya', age: 31, status: false},
-//     {id: 2, name: 'petya', age: 30, status: true},
-//     {id: 3, name: 'kolya', age: 29, status: true},
-//     {id: 4, name: 'olya', age: 28, status: false}
-// ];
-//
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ];
-// let usersWithCities = [];
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false}
+];
 
-// for (let i;i<usersWithId.length;i++){
-//     usersWithCities[usersWithCities.length] = usersWithId[i];
-//     console.log(usersWithCities);
-// }
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
+let usersWithCities = [];
+
+for (usersWithCities of usersWithId){
+    usersWithCities.address = true;
+    console.log(usersWithCities)
+}
