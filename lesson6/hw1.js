a68e9c23926cfc287320599917e148ef3cd873cd
NadiyPro <siroviyn13@gmail.com>
2024-05-15 14:20:18 +0300

    create hw1

--- lesson6/hw1.js
+++ lesson6/hw1.js
@@ -23,5 +23,18 @@ console.log(clean2);
 // //     let str = 'Ревуть воли як ясла повні';
 // // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні'];
 let str = 'Ревуть воли як ясла повні';
-let arr
-
+let arr = str.split();
+console.log(arr);
+// // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+let iter = [10,8,-7,55,987,-1011,0,1050,0];
+let mapIter = iter.map(value => `${value}`);
+console.log(mapIter);
+// // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+// // let nums = [11,21,3];
+// // sortNums(nums,'ascending') // [3,11,21]
+// // sortNums(nums,'descending') // [21,11,3]
+let nums = [11,21,3];
+let sortAsc = nums.toSorted((num1, num2)=>num1-num2);
+console.log(sortAsc);
+let sortDes = nums.toSorted((num1, num2)=>num2-num1);
+console.log(sortDes);
