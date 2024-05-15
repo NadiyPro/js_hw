e6c7bd6f55c00162cd35af4dcda6c5a20e5dc79f
NadiyPro <siroviyn13@gmail.com>
2024-05-15 15:44:00 +0300

    create hw2

--- lesson6/hw3.js
+++ lesson6/hw3.js
@@ -14,7 +14,7 @@
 let cardSuit = ['spade', 'diamond','heart', 'clubs'];
 let value = ['6','7','8','9','10','ace','jack','queen','king'];
 let color = ['red','black'];
-let cards = value.forEach(item => {item.concat(cardSuit)}) //value.concat(cardSuit); //value.forEach (item => value.concat(cardSuit));
+let cards = value.concat(cardSuit); //value.concat(cardSuit); //value.forEach (item => value.concat(cardSuit));
 // let cards = cardSuit.reduce((acc,index) =>{ if (acc.){
 //
 // }},[[],[]])
