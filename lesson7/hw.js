7271ab13fe119ae0033c4b79d9252d34774673a6
NadiyPro <siroviyn13@gmail.com>
2024-05-19 18:04:42 +0300

    create hw

--- lesson7/hw.js
+++ lesson7/hw.js
@@ -32,3 +32,23 @@ let sortUser= newUser.toSorted((iter1,iter2)=>iter1.id-iter2.id);
 console.log(sortUser);
 // // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 // // створити пустий масив, наповнити його 10 об'єктами Client
+class Client extends User {
+
+    constructor(id,name,surname,email,phone,order) {
+        super (id,name,surname,email,phone);
+        this.order = order
+    }
+}
+let clients = [];
+let client1 = new Client (1, 'vasya','Graham','Sincere@april.biz','380636632510', ['apple', 'orange', 'plum']);
+let client2 = new Client (2, 'petya','Howell','Shanna@melissa.tv','380636632511');
+let client3 = new Client (3, 'kolya','Bauch','Nathan@yesenia.net','380636632512');
+let client4 = new Client (4, 'olya','Lebsack','Julianne.OConner@kory.org','380636632513');
+let client5 = new Client (5, 'max','Dietrich','Lucio_Hettinger@annie.ca','380636632514');
+let client6 = new Client (6, 'anya','Schulist','Karley_Dach@jasper.info','380636632515');
+let client7 = new Client (7, 'oleg','Weissnat','Telly.Hoeger@billy.biz','380636632516');
+let client8 = new Client (8, 'andrey','Runolfsdottir','Sherwood@rosamond.me','380636632517');
+let client9 = new Client (9, 'masha','Reichert','Chaim_McDermott@dana.io','380636632518');
+let client10 = new Client (10, 'olya','DuBuqu','Rey.Padberg@karina.biz','380636632519');
+clients.push(client1,client2,client3,client4,client5,client6,client7,client8,client9,client10);
+console.log(clients);
