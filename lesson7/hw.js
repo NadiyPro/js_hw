f27e5af257f90f2c0672ede70c9c7b454721cc74
NadiyPro <siroviyn13@gmail.com>
2024-05-19 17:15:22 +0300

    create hw

--- lesson7/hw.js
+++ lesson7/hw.js
@@ -1,4 +1,4 @@
-// // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// // - Створити функцію конструктор для об'єктів User з полями id, name, surname, email, phone
 // // створити пустий масив, наповнити його 10 об'єктами new User(....)
 function User (id,name,surname,email,phone) {
         this.id= id;
@@ -21,42 +21,10 @@ let user10 = new User(10, 'olya','DuBuqu','Rey.Padberg@karina.biz','380636632519
 newUser.push(user1,user2,user3,user4,user5,user6,user7,user8,user9,user10);
 console.log(newUser)
 // // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
-//
-debugger;
-// let idFilter = newUser.filter(iter=>{
-//     for(let value in iter){
-//         if(value.id % 2===0){
-//             console.log(idFilter)
-//         }
-//     }
-// })
-
 let idFilter = newUser.filter(iter=>{
-    for(let value in iter){
-        for(let valueId in value){
-            if(valueId.id % 2===0){
-                console.log(idFilter)
-            }
+        if(iter.id % 2===0){
+            return iter;
         }
-    }
-})
-
-// User.prototype.idFilter = newUser.filter(iter=>{
-//         for(let value in iter){
-//             for(let valueId in value)
-//             if(value.id % 2===0){
-//                 console.log(valueId)
-//             }
-//         }
-//     })
-
-// User.prototype.idFilter = newUser.filter(iter=>{
-//     for(let i;i<iter.length;i++){
-//             // if(value.id % 2===0){
-//             //     value[id] = iter.id;
-//                 console.log(iter)
-//             }
-//     }
-// )
-// console.log(idFilter)
+});
+console.log(idFilter)
 
