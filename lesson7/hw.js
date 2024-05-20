76167ab4a8cba0e94e535446918388df68ac21c1
NadiyPro <siroviyn13@gmail.com>
2024-05-20 14:50:38 +0300

    create hw

--- lesson7/hw.js
+++ lesson7/hw.js
@@ -226,11 +226,11 @@ let arrayFilter = [
     {name: 'kolya', age: 29},
     {name: 'olya', age: 28},
     {name: 'max', age: 30},
-    {name: 'anya', age: 31, },
+    {name: 'anya', age: 31},
     {name: 'oleg', age: 28},
     {name: 'andrey', age: 29},
-    {name: 'masha', age: 30 },
-    {name: 'max', age: 31 }
+    {name: 'masha', age: 30},
+    {name: 'max', age: 31}
 ];
 Array.prototype.myFilter = function (callback){
     for(let itemFilter of this){
@@ -240,4 +240,5 @@ Array.prototype.myFilter = function (callback){
     }
 }
 arrayFilter.myFilter((value)=>value.age===30);
+// // ******************* map ******************** // //
 
