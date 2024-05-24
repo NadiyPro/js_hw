e54cf62ec77b1d37013e9799dd46a5ba9aa1eb7f
NadiyPro <siroviyn13@gmail.com>
2024-05-24 14:33:21 +0300

    create hw.

--- lesson9/hw.js
+++ lesson9/hw.js
@@ -31,3 +31,27 @@ for (let i=0;i<arrayUl.length;i++) {
     ul.append(li);
     console.log(li)
 }
+// // - Є масив
+// // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// // Завдання робити через цикли.
+    let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (let divCour of coursesAndDurationArray) {
+    let divCour = document.createElement('div');
+    divCour.innerHTML=`title: ${divCour.title}, monthDuration: ${divCour.monthDuration}`;
+    document.body.append(divCour);
+    console.log(divCour);
+}
+// for (let i=0;i<coursesAndDurationArray.length;i++) {
+//     let divCour = document.createElement('div');
+//     // let li = document.createElement('li');
+//     divCour.innerHTML=coursesAndDurationArray[i];
+//     document.body.append(divCour);
+//     console.log(divCour)
+// }
