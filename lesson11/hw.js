28b46875bbb58b87f4a42ba127d162eca8cc1865
NadiyPro <siroviyn13@gmail.com>
2024-05-31 19:17:39 +0300

    create hw.

--- lesson11/hw.js
+++ lesson11/hw.js
@@ -13,22 +13,25 @@ fetch('https://dummyjson.com/carts')
             for(let cart of carts){
                 let cartDiv=document.createElement('div');
                 cartDiv.classList.add('cartDiv');
-                let idBasicH3=document.createElement('h3');
-                idBasicH3.classList.add('idBasicH3')
-                idBasicH3.innerHTML=`${cart.id}`
-                    for (let product of cart.products) {
+                // let idBasicH3=document.createElement('h3');
+                // idBasicH3.classList.add('idBasicH3')
+                // idBasicH3.innerHTML=`${cart.id}`
+                cartDiv.innerHTML=`<h2>id: ${cart.id} </h2> <p> total: ${cart.total} <br>  discountedTotal: ${cart.discountedTotal} <br> totalProducts: ${cart.totalProducts} <br>  totalQuantity: ${cart.totalQuantity} <br> userId: ${cart.userId} </p> `
+                for (let product of cart.products) {
                         let productsDiv = document.createElement('div');
                         productsDiv.classList.add('productsDiv')
                         let img = document.createElement('img');
                         img.classList.add('img')
                         img.src=product.thumbnail;
-                        productsDiv.innerHTML=`<h2>${product.title}</h2> <p>discountPercentage: ${product.discountPercentage} <br> discountedTotal: ${product.discountedTotal} <br> id:${product.id} <br> price: ${product.price} <br> quantity:${product.quantity}</p> `
+                        productsDiv.innerHTML=`<h3>${product.title}</h3> <p>discountPercentage: ${product.discountPercentage} <br> discountedTotal: ${product.discountedTotal} <br> id:${product.id} <br> price: ${product.price} <br> quantity:${product.quantity}</p> `
                         productsDiv.append(img);
-                        idBasicH3.append(productsDiv);
+                    cartDiv.append(productsDiv);
                     }
+
                 basicDiv.append(cartDiv)
-                cartDiv.append(idBasicH3)
+                // cartDiv.append(idBasicH3)
             }
+
     })
 // .catch(
 //     (products)=>console.log('image is missing id 25')
