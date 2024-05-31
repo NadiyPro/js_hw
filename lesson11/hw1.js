a93d0586ad32143ed983032fbd1b81506e633100
NadiyPro <siroviyn13@gmail.com>
2024-05-31 20:46:26 +0300

    create hw.

--- lesson11/hw1.js
+++ lesson11/hw1.js
@@ -7,14 +7,14 @@ fetch('https://dummyjson.com/carts')
     .then((response)=>response.json())
     .then(({carts})=>{
             for(let cart of carts){
-                let cartDiv=document.createElement('div');
+                const cartDiv=document.createElement('div');
                 cartDiv.classList.add('cartDiv');
                 cartDiv.innerHTML=`<h2>Basket id: ${cart.id} </h2> <br> <p> total: ${cart.total} <br>  discountedTotal: ${cart.discountedTotal} <br> totalProducts: ${cart.totalProducts} <br>  totalQuantity: ${cart.totalQuantity} <br> userId: ${cart.userId} </p> `
 
                 for (let product of cart.products) {
-                        let productsDiv = document.createElement('div');
+                    const productsDiv = document.createElement('div');
                         productsDiv.classList.add('productsDiv')
-                        let img = document.createElement('img');
+                    const img = document.createElement('img');
                         img.classList.add('img')
                         img.src=product.thumbnail;
                         productsDiv.innerHTML=`<h3>${product.title}</h3> <p>discountPercentage: ${product.discountPercentage} <br> discountedTotal: ${product.discountedTotal} <br> id:${product.id} <br> price: ${product.price} <br> quantity:${product.quantity}</p> `
--- lesson11/hw2.js
+++ lesson11/hw2.js
@@ -1,4 +1,25 @@
 // // - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+const basicDiv = document.createElement('div');
+basicDiv.classList.add('basicDiv');
+document.body.append(basicDiv)
 fetch('https://dummyjson.com/recipes')
     .then ((response)=>response.json())
-    .then(({recipes})=>console.log(recipes))
+    .then(({recipes})=>{console.log(recipes)
+    })
+fetch('https://dummyjson.com/recipes')
+    .then ((response)=>response.json())
+    .then(({recipes})=>{
+        for(let description of recipes){
+            const innerDiv = document.createElement('div');
+            innerDiv.classList.add('innerDiv');
+            basicDiv.append(innerDiv)
+
+            const hDiv = document.createElement('div');
+            hDiv.classList.add('hDiv');
+            hDiv.innerHTML=`<h2>id: ${description.id} <br> ${description.name} </h2> <p> cuisine: ${description.cuisine} <br> difficulty: ${description.difficulty} <br> caloriesPerServing: ${description.caloriesPerServing} <br> cookTimeMinutes: ${description.cookTimeMinutes}</p>`
+            const
+            innerDiv.append(hDiv)
+
+
+        }
+    })
