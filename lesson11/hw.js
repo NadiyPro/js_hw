e4ef8f8dad5cbdca89ef9240fc19aa94462581bb
NadiyPro <siroviyn13@gmail.com>
2024-05-31 19:55:09 +0300

    create hw.

--- lesson11/hw.js
+++ lesson11/hw.js
@@ -13,10 +13,8 @@ fetch('https://dummyjson.com/carts')
             for(let cart of carts){
                 let cartDiv=document.createElement('div');
                 cartDiv.classList.add('cartDiv');
-                // let idBasicH3=document.createElement('h3');
-                // idBasicH3.classList.add('idBasicH3')
-                // idBasicH3.innerHTML=`${cart.id}`
-                cartDiv.innerHTML=`<h2>id: ${cart.id} </h2> <p> total: ${cart.total} <br>  discountedTotal: ${cart.discountedTotal} <br> totalProducts: ${cart.totalProducts} <br>  totalQuantity: ${cart.totalQuantity} <br> userId: ${cart.userId} </p> `
+                cartDiv.innerHTML=`<h2>Basket id: ${cart.id} </h2> <br> <p> total: ${cart.total} <br>  discountedTotal: ${cart.discountedTotal} <br> totalProducts: ${cart.totalProducts} <br>  totalQuantity: ${cart.totalQuantity} <br> userId: ${cart.userId} </p> `
+
                 for (let product of cart.products) {
                         let productsDiv = document.createElement('div');
                         productsDiv.classList.add('productsDiv')
@@ -27,11 +25,8 @@ fetch('https://dummyjson.com/carts')
                         productsDiv.append(img);
                     cartDiv.append(productsDiv);
                     }
-
                 basicDiv.append(cartDiv)
-                // cartDiv.append(idBasicH3)
             }
-
     })
 // .catch(
 //     (products)=>console.log('image is missing id 25')
