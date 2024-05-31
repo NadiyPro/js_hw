f33ec3bcce3d04aa0af0011fca1aa138f0c3d1a0
NadiyPro <siroviyn13@gmail.com>
2024-05-31 20:19:23 +0300

    create hw.

---
+++ lesson11/hw2.js
@@ -0,0 +1,4 @@
+// // - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+fetch('https://dummyjson.com/recipes')
+    .then ((response)=>response.json())
+    .then(({recipes})=>console.log(recipes))
