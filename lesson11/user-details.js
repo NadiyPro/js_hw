effa13d67b8b1e016fe3d0e832b3c94f4c538a87
NadiyPro <siroviyn13@gmail.com>
2024-06-01 13:59:26 +0300

    create hw.

--- lesson11/user-details.js
+++ lesson11/user-details.js
@@ -12,9 +12,29 @@ window.onload=function () {
     const urlParams = new URLSearchParams(window.location.search);
     const userId = urlParams.get('id')
 
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('innerDiv');
+    innerDiv.innerHTML = `<h3>${userId}</h3>`
+    detailsDiv.append(innerDiv);
+
     fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
     .then((response)=>response.json())
-    .then((userId)=>console.log(userId))
+    .then((user)=>console.log(user))
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((response)=>response.json())
+        .then((user)=>{
+            innerDiv.innerHTML=`<h2>name: ${user.name} <br> username: ${user.username}</h2> <p>website: ${user.website} <br> phone: ${user.phone} <br> website: ${user.website}</p>`
+
+            const divAddress = document.createElement('div');
+            divAddress.innerHTML=`divAddress:`
+            divAddress.innerHTML=`<p>address: <br> city: ${user.address.city} <br> street: ${user.address.street} <br> suite: ${user.address.suite} <br> zipcode: ${user.address.zipcode} <br> geo: lat ${user.address.geo.lat}, lng ${user.address.geo.lng}</p>`
+            console.log(user.address.geo.lat, user.address.geo.lng)
+
+            innerDiv.append(divAddress);
+
+            }
+        )
 
 }
 
